996c4bc8485bc0d5c3afb1a5a781a6f73d24a3f5
Benedict Armstrong <contact@benarmstro.ng>
2023-11-22 18:52:12 +0100

    better performance

--- lab05/mean-shift/animate.py
+++ lab05/mean-shift/animate.py
@@ -1,3 +1,4 @@
+import os
 from mean_shift import meanshift_step
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,27 +10,82 @@ from skimage.transform import rescale
 
 
 scale = 0.5    # downscale the image to run faster
+image_path = "eth.jpg"
+image_name, _ = os.path.splitext(image_path)
+steps = 15
+bandwidth = 1
+with_color = True
+distance = False
+
 
 # Load image and convert it to CIELAB space
-image = rescale(io.imread('eth.jpg'), scale, channel_axis=-1)
+image = rescale(io.imread(image_path), scale, channel_axis=-1)
 image_lab = color.rgb2lab(image)
 shape = image_lab.shape  # record image shape
-image_lab = image_lab.reshape([-1, 3])  # flatten the image
+if distance:
+    image_lab = np.concatenate(
+        (image_lab, np.indices(shape[:2]).transpose(1, 2, 0)),
+        axis=2
+    )
+
+    image_lab[:, :, 3:] *= 0.2
+
+    # flatten the image with 5 channels
+    image_lab = image_lab.reshape([-1, 5])
+else:
+    image_lab = image_lab.reshape([-1, 3])  # flatten the image
 
 X = image_lab
 
 
 def update(i):
     global X
-    print(f"Step {i}")
-    X = meanshift_step(X)
-    scatter.set_array([X[:, 0], X[:, 1], X[:, 2]])
+    global scatter
+    print(f"animation step {i}")
+    cache_file = f".cache/{image_name}/ms_{'location_0.2_' if distance else ''}{bandwidth}_{i}.npz"
+    if os.path.exists(cache_file):
+        print(f"loading from {cache_file}")
+        X = np.load(cache_file)['X']
+    else:
+        print(f"running meanshift_step {i}")
+        X = meanshift_step(X, bandwidth=bandwidth)
+
+    # update scatter convert CIELAB color to RGB color
+    scatter.set_offsets(X[:, :2])
+    scatter.set_3d_properties(X[:, 2], 'z')
+    if with_color:
+        scatter.set_color(color.lab2rgb(X[:, :3]))
 
 
-fig, ax = plt.subplots()
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.title.set_text(
+    f"bandwidth={bandwidth}, steps={steps}{', with physical distance' if distance else ''}")
+ax.set_xlabel('L')
+ax.set_ylabel('a')
+ax.set_zlabel('b')
+# ax.set_xlim(0, 100)
+# ax.set_ylim(-128, 127)
+# ax.set_zlim(-128, 127)
 # plot 3D scatter of X
 scatter = ax.scatter(X[:, 0], X[:, 1], X[:, 2])
 
+if distance:
+    # set color based on position in RGB color space r,g,b -> [0, 1]
+    c = X[:, 3:] * 50
 
-ani = animation.FuncAnimation(fig, update, frames=19, interval=500)
+    # add a column of zeros to make it 3D
+    c = np.concatenate((c, np.ones((c.shape[0], 1)) * 0.5), axis=1)
+
+    # make sure all values are in [0, 1] range by scaling down (for both axis independently)
+    c[:, 0] /= np.max(c[:, 0])
+    c[:, 1] /= np.max(c[:, 1])
+
+    scatter.set_color(c)
+
+ani = animation.FuncAnimation(fig, update, frames=steps)
+
+ani.save(f"images/{image_name}/{'location_' if distance else ''}{'color_' if with_color else''}{bandwidth}_{steps}.gif",
+         writer='imagemagick', fps=3)
+
 plt.show()
--- lab05/mean-shift/mean_shift.py
+++ lab05/mean-shift/mean_shift.py
@@ -44,17 +44,21 @@ def meanshift_step(X: np.array, bandwidth=2) -> np.array:
 
 
 def meanshift(X: np.array, bandwidth=2, steps=5) -> np.array:
-    for i in range(steps):
+    for _ in range(steps):
         X = meanshift_step(X, bandwidth=bandwidth)
     return X
 
 
-def test_meanshift(bandwidth=5, steps=10, scale=0.5, location_penalty=None):
+def test_meanshift(image_path: str, bandwidth=5, steps=10, scale=0.5, location_penalty=None):
 
-    name = f"{f'xy_{location_penalty}_' if location_penalty else '' }{bandwidth}_{steps}"
+    image_name, _ = os.path.splitext(image_path)
+    if not os.path.isdir(f'images/{image_name}'):
+        os.makedirs(f'images/{image_name}')
+    if not os.path.isdir(f'.cache/{image_name}'):
+        os.makedirs(f'.cache/{image_name}')
 
     # Load image and convert it to CIELAB space
-    original_image = io.imread('eth.jpg')
+    original_image = io.imread(image_path)
     image = rescale(original_image, scale, channel_axis=-1)
     image_lab = color.rgb2lab(image)
     shape = image_lab.shape  # record image shape
@@ -73,64 +77,77 @@ def test_meanshift(bandwidth=5, steps=10, scale=0.5, location_penalty=None):
         # flatten the image with 3 channels
         image_lab = image_lab.reshape([-1, 3])
 
-    # Run your mean-shift algorithm
-    t = time.time()
-    cache_file = f'.cache/ms_{name}.npz'
-    if os.path.exists(cache_file):
-        data = np.load(cache_file)
-        X = data['X']
-    else:
-        X = meanshift(image_lab, bandwidth=bandwidth, steps=steps)
-        np.savez(cache_file, X=X)
-    t = time.time() - t
-    print('Elapsed time for mean-shift: {}'.format(t))
+    X = image_lab
+    for i in range(steps):
 
-    # Load label colors and draw labels as an image
-    colors = np.load('colors.npz')['colors']
-    colors_2 = np.load('colors_2.npz')['colors']
-    colors[colors > 1.0] = 1
-    colors[colors < 0.0] = 0
+        name = f"{f'location_{location_penalty}_' if location_penalty else '' }{bandwidth}_{i}"
 
-    centroids, labels = np.unique((X / 4).round(), return_inverse=True, axis=0)
+        # Run your mean-shift algorithm
+        t = time.time()
+        cache_file = f'.cache/{image_name}/ms_{name}.npz'
+        if os.path.exists(cache_file):
+            data = np.load(cache_file)
+            X = data['X']
+        else:
+            X = meanshift_step(X, bandwidth=bandwidth)
+            np.savez(cache_file, X=X)
 
-    print(
-        f"\tFound: {len(centroids)} centroids in {steps} steps with bandwidth {bandwidth}")
+        t = time.time() - t
+        print('Elapsed time for mean-shift: {}'.format(t))
 
-    if len(centroids) > 500:
-        print("\t\tToo many centroids, skipping")
-        return
-    if len(centroids) > 23:
-        result_image = colors_2[labels].reshape(shape)
-    else:
-        result_image = colors[labels].reshape(shape)
+        # Load label colors and draw labels as an image
+        colors = np.load('colors.npz')['colors']
+        colors_2 = np.load('colors_2.npz')['colors']
+        colors[colors > 1.0] = 1
+        colors[colors < 0.0] = 0
 
-    # resize result image to original resolution
-    result_image = rescale(result_image, 1 / scale, order=0, channel_axis=-1)
-    result_image = (result_image * 255).astype(np.uint8)
-    io.imsave(f'images/result_{name}.png', result_image)
+        centroids, labels = np.unique(
+            (X / 4).round(), return_inverse=True, axis=0)
 
-    # draw outline of segmentation result on original image (with original resolution)
-    image_outlined = np.copy(image)
-    for i in range(shape[0]):
-        for j in range(shape[1]):
-            if i > 0 and labels[i * shape[1] + j] != labels[(i - 1) * shape[1] + j]:
-                image_outlined[i, j] = [255, 0, 0]
-            elif j > 0 and labels[i * shape[1] + j] != labels[i * shape[1] + j - 1]:
-                image_outlined[i, j] = [255, 0, 0]
-    result_image = rescale(image_outlined, 1 / scale,
-                           order=0, channel_axis=-1)
-    result_image = (result_image * 255).astype(np.uint8)
-    io.imsave(
-        f'images/outlined_{name}.png', result_image)
+        print(
+            f"\tFound: {len(centroids)} centroids in {i} steps with bandwidth {bandwidth}")
+
+        if len(centroids) > 500:
+            print("\t\tToo many centroids, skipping")
+            continue
+        if len(centroids) > 23:
+            result_image = colors_2[labels].reshape(shape)
+        else:
+            result_image = colors[labels].reshape(shape)
+
+        # resize result image to original resolution
+        result_image = rescale(result_image, 1 / scale,
+                               order=0, channel_axis=-1)
+        result_image = (result_image * 255).astype(np.uint8)
+        io.imsave(f'images/{image_name}/result_{name}.png', result_image)
+
+        # draw outline of segmentation result on original image (with original resolution)
+        image_outlined = np.copy(image)
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+                if i > 0 and labels[i * shape[1] + j] != labels[(i - 1) * shape[1] + j]:
+                    image_outlined[i, j] = [255, 0, 0]
+                elif j > 0 and labels[i * shape[1] + j] != labels[i * shape[1] + j - 1]:
+                    image_outlined[i, j] = [255, 0, 0]
+        result_image = rescale(image_outlined, 1 / scale,
+                               order=0, channel_axis=-1)
+        result_image = (result_image * 255).astype(np.uint8)
+        io.imsave(
+            f'images/{image_name}/outlined_{name}.png', result_image)
 
 
 if __name__ == '__main__':
-    steps = range(1, 15, 1)
-    bandwidth = [7]
+    steps = 15
+    bandwidth = [3, 5]
 
     # run for all combinations of steps and bandwidth
-    for s in steps:
-        for b in bandwidth:
-            print(
-                f'Running mean-shift with bandwidth={b}, steps={s}')
-            test_meanshift(bandwidth=b, steps=s)
+    for b in bandwidth:
+        print(
+            f'Running mean-shift with bandwidth={b}, steps={steps}')
+        test_meanshift(
+            "eth.jpg",
+            bandwidth=b,
+            steps=steps,
+            scale=0.5,
+            location_penalty=0.2
+        )
